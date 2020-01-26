d544235010ba24af491191e869041206e1c23b7d
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-26 15:50:40 -0500

    Server boilerplate (express, mongoose, "./routes")

---
+++ server.js
@@ -0,0 +1,37 @@
+/* Dependencies */
+/* -------------------------------*/
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+/* Global variables and instances */
+/* -------------------------------*/
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+/* MAIN */
+/* -------------------------------*/
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+// API routes
+app.use(routes);
+
+// Mongoose
+mongoose.connect('mongodb://localhost/eagle_talk_db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
