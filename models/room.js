d0b569f5d8a08aabbf6d5c7a0de00c4ca56f9e6f
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-27 04:16:49 -0500

    Swapped "code" and "name" (now "name" is first)

--- models/room.js
+++ models/room.js
@@ -9,8 +9,8 @@ const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
 	// All required
-	code: { type: String, required: true },
 	name: { type: String, required: true },
+	code: { type: String, required: true },
 	capacity: { type: Number, required: true },
 	masterPassword: { type: String, required: true },
 });
