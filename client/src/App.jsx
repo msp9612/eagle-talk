49a8d09a284d2d480910c87f35cc6ab53cb18f7a
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-26 19:50:26 -0500

    Initial App.jsx with sample code (pre-routing)

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+/* Dependencies */
+/* -------------------------------*/
+
+// import React from 'react';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import Nav from './components/Nav';
+// import Score from './components/Score';
+// import GameBoard from './components/GameBoard';
+// import PlayAgain from './components/PlayAgain';
+// import './AppStyles.scss';
+import React from 'react';
+
+/* MAIN */
+/* -------------------------------*/
+
+// export default () => (
+// 	<React.Fragment>
+// 		<Nav />
+// 		<Container className='mt-1' fluid={true}>
+// 			<Row>
+// 				<Col>
+// 					<Score />
+// 				</Col>
+// 			</Row>
+// 			<Row className='mt-4'>
+// 				<Col>
+// 					<GameBoard />
+// 				</Col>
+// 			</Row>
+// 		</Container>
+// 		<PlayAgain />
+// 	</React.Fragment>
+// );
+export default () => (
+	<React.Fragment>
+		<h1>Eagle Talk testing: App.jsx</h1>
+	</React.Fragment>
+);
