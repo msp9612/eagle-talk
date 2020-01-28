bd82856e9a5bcdf9f9a53c69967e91067dae5b9e
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-28 02:40:18 -0500

    "RoomQueue" to "Queue" (name change)

---
+++ client/src/components/Queue.jsx
@@ -0,0 +1,28 @@
+/* Dependencies */
+/* -------------------------------*/
+
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+/* Global variables and instances */
+/* -------------------------------*/
+
+const Queue = props => {
+	return (
+		<ListGroup className='text-center'>
+			<ListGroup.Item>Person B</ListGroup.Item>
+			<ListGroup.Item>Person C</ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+			<ListGroup.Item></ListGroup.Item>
+		</ListGroup>
+	);
+};
+
+/* MAIN */
+/* -------------------------------*/
+
+export default Queue;
