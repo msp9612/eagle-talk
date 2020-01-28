26d0fbdbddd42493fe8222fa9a78fd75081c6667
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-27 21:37:19 -0500

    Removed unneeded useEffect

--- client/src/components/RoomJumbotron.jsx
+++ client/src/components/RoomJumbotron.jsx
@@ -3,22 +3,11 @@
 
 import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
-import { useEffect } from 'react';
 
 /* Global variables and instances */
 /* -------------------------------*/
 
 const RoomJumbotron = props => {
-	useEffect(() => {
-		const script = document.createElement('script');
-		script.src = '/socket.io/socket.io.js';
-		document.body.appendChild(script);
-		return () => {
-			console.log('hi');
-			document.body.removeChild(script);
-		};
-	}, []);
-
 	return (
 		<Jumbotron className='text-center'>
 			<Container>
